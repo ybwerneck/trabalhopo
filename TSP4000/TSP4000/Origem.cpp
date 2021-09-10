d52d104d32dca7536eb5cca2add73cdbed4999dd
yan bw <yanbwerneck@outlook.com>
2021-09-10 17:20:00 -0300

    bug +=

--- TSP4000/TSP4000/Origem.cpp
+++ TSP4000/TSP4000/Origem.cpp
@@ -1,6 +1,5 @@
 #include<iostream> 
 #include"ilcplex/ilocplex.h";
-#include"ilconcert/ilolinear.h";
 #include <cmath>
 #include <limits>
 
@@ -29,110 +28,111 @@ int countLines(string s) {
 	file.close();
 	return nc;
 }
-int loadFile(string s,int*** c) {
-	
-	 std::string line;
-	 int val;
-	 int nc = countLines(s);
-	 (*c) = new int* [nc];
-	 int lineIdx = 0;
-	 std::ifstream myFile(s);
-	 while (std::getline(myFile, line))
-	 {
-		 std::stringstream ss(line);
-		 int colIdx = 0;
-	     int* aux = new int[nc];
-		 while (ss >> val) {
-			 aux[colIdx] = val;
-			 if (ss.peek() == ',') ss.ignore();
-			 colIdx++;
-		 }
-		 (*c)[lineIdx++] = aux;
-	
-	 }
-	 myFile.close();
-	 return nc;
-}
+int loadFile(string s, int*** c) {
 
+	std::string line;
+	int val;
+	int nc = countLines(s);
+	(*c) = new int*[nc];
+	int lineIdx = 0;
+	std::ifstream myFile(s);
+	while (std::getline(myFile, line))
+	{
+		std::stringstream ss(line);
+		int colIdx = 0;
+		int* aux = new int[nc];
+		while (ss >> val) {
+			aux[colIdx] = val;
+			if (ss.peek() == ',') ss.ignore();
+			colIdx++;
+		}
+		(*c)[lineIdx++] = aux;
+
+	}
+	myFile.close();
+	return nc;
+}
 int main()
 {
-	// criacao do ambiente
 	IloEnv env;
-
-	// criacao do objeto modelo
 	IloModel modelo(env);
-	
+
+#pragma region Dados	
+
+	//Matriz de distancias
+	int** C;
+	int n = loadFile("a.csv", &C);
+	int nd = n;
+	printMatrix(n, C);
+
+	/*
+	C[0] = new int[nD] {0 , 2,29,22,13,24};
+	C[1] = new int[nD] {12, 0,19, 3,25, 6};
+	C[2] = new int[nD] {29,19,0 ,21,23,25};
+	C[3] = new int[nD] {22, 3,21, 0, 4, 5};
+	C[4] = new int[nD] {13,25,23, 4, 0,16};
+	C[5] = new int[nD] {24, 6,28, 5,16, 0};
+	*/
+#pragma endregion
+
+
 #pragma region Variaveis de Decisao
 
-	// variavel de custo	
-	int** C; 
-	int n; //tamanho dos vetores
-	n = loadFile("a.csv", &C); //carrega dados
-
-	// vetor de variaveis binarias
-	IloArray<IloNumVarArray> X(env);
+	// variaveis de decisao
+	IloArray<IloNumVarArray> X(env, n);
 	IloNumVarArray t(env, n);
-	
+
+	// Restrições 1, 2 e 3 respectivamente
+	IloRangeArray arco_entrada(env, n);
+	IloRangeArray arco_saida(env, n);
+	IloArray<IloRangeArray> mtz(env, n);
+
+	// variavel fixa em valor 1
+	t[0] = IloNumVar(env, 1, 1, IloNumVar::Int);
+
+	// Criar variavel t[1],...., t[n]
+	for (int i = 1; i < n; i++) {
+		t[i] = IloNumVar(env, 2, n, IloNumVar::Int);
+	}
+
 	// Cria variaveis x binárias	
-	for (int i = 0; i < n; i++) {		
-		for (int j = 0; j < n; j++) {
-			X[i][j] = IloNumVar(env, 0, 1, ILOBOOL);
+	for (int i = 0; i < n; i++) {
+		X[i] = IloNumVarArray(env, n);
+		for (auto j = 0u; j < n; ++j) {
+			X[i][j] = IloNumVar(env, 0, 1, IloNumVar::Bool);
 		}
 	}
 #pragma endregion
 
-	// cria expressões para o modelo
 	IloExpr expr(env);
 
 #pragma region Restricoes
 
-	// Restrições 1, 2 e 3 respectivamente
-	IloRangeArray entrada(env, n);
-	IloRangeArray saida(env, n);
-	IloArray<IloRangeArray> mtz(env, n);
-
-	// Restricao 1 (fluxo de entrada)
+	// Restricao 1
 	for (int i = 0; i < n; i++) {
+		expr = IloExpr(env);
 		for (int j = 0; j < n; j++) {
-			if (i != j) {
-				expr += X[j][i];
-			}
+			expr += X[j][i];
 		}
-		entrada[i] = IloRange(env, 1, expr, 1);
+		arco_entrada[i] = IloRange(env, 1, expr, 1);
 	}
 
 	// Adiciona restricao 1 ao modelo
-	modelo.add(entrada);
+	modelo.add(arco_entrada);
 
-	// Restricao 2 (fluxo de saida)
+	// Restricao 2
 	for (int i = 0; i < n; i++) {
+		expr = IloExpr(env);
 		for (int j = 0; j < n; j++) {
-			if (i != j) {
-				expr += X[i][j];
-			}
+			expr += X[i][j];
 		}
-		saida[i] = IloRange(env, 1, expr, 1);
+		arco_saida[i] = IloRange(env, 1, expr, 1);
 	}
 
 	// Adiciona restricao 2 ao modelo
-	modelo.add(saida);	
+	modelo.add(arco_saida);
 
-	// Create constraints 3)
-   // The constraint is for i = 1,...,n and therefore we add empty constraints for i == 0
-	mtz[0] = IloRangeArray(env);
-	// We then continue normally for all other i > 0
-	for (auto i = 1u; i < n; ++i) {
-		mtz[i] = IloRangeArray(env, n);
-		for (auto j = 1u; j < n; ++j) {
-			expr = t[i] - t[j] + static_cast<int>(n) * X[i][j];
-
-			
-			mtz[i][j] = IloRange(env, -IloInfinity, expr, n - 1);
-		}
-		// Add constraints 3)[i] to the model
-		modelo.add(mtz[i]);
-	}
-
+	// Restricao 3
 	// Restricao 3
 	// Restrição de i == 0
 	mtz[0] = IloRangeArray(env);
@@ -140,12 +140,15 @@ int main()
 	for (int i = 1; i < n; i++) {
 		mtz[i] = IloRangeArray(env, n);
 		for (int j = 1; j < n; j++) {
-			expr = t[i] - t[j] + static_cast<int>(n) * X[i][j];			
+			expr = t[i] - t[j] + static_cast<int>(n) * X[i][j];
 			mtz[i][j] = IloRange(env, -IloInfinity, expr, n - 1);
 		}
 		// Adiciona restricao 3 ao modelo
 		modelo.add(mtz[i]);
 	}
+
+	// Restrição de i == 0
+	
 
 #pragma endregion
 
@@ -158,29 +161,25 @@ int main()
 	}
 	IloObjective obj = IloMinimize(env, expr);
 
-#pragma endregion
-
 	// Adiciona funcao objetivo ao modelo
 	modelo.add(obj);
 
-	// Finaliza expressões, desalocando memória
+	// Finaliza expr
 	expr.end();
 
-	// Cria o objeto conversor para a linguagem cplex
-	IloCplex cplex(modelo);	
+#pragma endregion
 
-	//imprime matriz de distancias (custos)
-	printMatrix(n, C);
+	// Cria o objeto solver
+	IloCplex cplex(modelo);
 
 	try {
 		cplex.extract(modelo);
+		cplex.exportModel("aName.lp");
 		cplex.solve();
-		double result = cplex.getObjValue(); //retorna valor da função objetivo
-		cout << "Valor da funcao objetivo: " << result;
-		cout << "\n";
+
+		cout << "Valor do objetivo: " << cplex.getObjValue() << "\n"; //retorna valor da função objetivo
 	}
 	catch (IloException& ex) {
-		cout << "Solucao nao encontrada" << endl;
+		cout << "\nSolucao nao existe :" <<ex.getMessage() <<endl;
 	}
-		
 }
